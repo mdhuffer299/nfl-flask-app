07852a9a4609f8c2849a889a2707872993dee956
Michael Huffer <mhuffer-m1@macbook-pro.local>
2022-08-18 20:36:21 -0400

    db connection client

--- utils/db_conn.py
+++ utils/db_conn.py
@@ -1,7 +1,40 @@
 import psycopg2
+import logging
 
 
-def get_db_connection():
-    conn = psycopg2.connect(host='localhost',
-                            database='postgres')
-    return conn
+class Client:
+    def __init__(self, host='localhost', database='postgres'):
+        self.host = host
+        self.database = database
+        self.conn = None
+        self.cursor = None
+        self.open_conn()
+
+    def open_conn(self):
+        try:
+            self.conn = psycopg2.connect(host=self.host,
+                                         database=self.database)
+            self.cursor = self.conn.cursor()
+        except psycopg2.OperationalError as err:
+            logging.error(err)
+
+        return self
+
+    def close_conn(self):
+        self.conn.close()
+        self.cursor.close()
+
+        return "Closed Connection"
+
+    def query(self, query):
+        db = self.open_conn()
+        db.cursor.execute(query)
+        query_results = db.cursor.fetchall()
+
+        return query_results
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
